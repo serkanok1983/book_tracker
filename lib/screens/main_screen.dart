f65028c8f2c76afb93eecac028e2e3c9675e2221
Serkan <serkanok1983@yahoo.com>
2021-07-05 23:46:58 +0300

    firestore init

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MainScreenPage extends StatelessWidget {
@@ -5,9 +6,39 @@ class MainScreenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Text('Hello world!'),
+    CollectionReference booksCollection =
+        FirebaseFirestore.instance.collection('books');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white24,
+        elevation: 0.0,
+        centerTitle: false,
+        title: Row(
+          children: [
+            Text(
+              'Okur',
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    color: Colors.redAccent,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream: booksCollection.snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                Object? data = snapshot.data!.docs.first.data();
+                return Text(
+                  (data! as Map)['author'],
+                  style: TextStyle(color: Colors.black),
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
